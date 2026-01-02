6948c7cb5fb708d641c112d6cbb166dc38cc1b79
Roman Danilov <neci.03@mail.ru>
2026-01-02 20:18:28 +0300

    GUI fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 use eframe::egui;
 use image::{DynamicImage, ImageBuffer, Rgba, RgbaImage};
 use ort::value::{Value, ValueType};
